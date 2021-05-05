affc9937c2a0d82a1a7d38242aa732e31a5c8077
cca923 <anna85923@gmail.com>
2021-05-05 15:12:52 +0800

    update 2021/05/05 -6

--- public/js/app.js
+++ public/js/app.js
@@ -62,27 +62,26 @@ weatherForm.addEventListener("submit", (e) => {
 });
 
 /////////////////////////////////////////////////////
-//宣告預設表單內容為空
-var initSubject = "",
-  initBody = "";
+const initSubject = "";
+const initBody = "";
 
-//按下傳送按鈕後執行
 function submitHandler() {
-  var to = "contact@example.com"; //寫死的傳送對象 就是公司的信箱 不會顯示在網頁上
-  var name = nameText.value; //讀取ID為 nameTextuser 物件中的值
-  var email = emailText.value;
-  var tel = telText.value;
-  var subject = subText.value;
-  //把user填的資料都塞到 mail body 中
-  var body = "" + bodyText.value + "%0A%0A%0A"; //%0A是換行 換了三行
+  const to = "anna85923@gmail.com";
+  const name = nameText.value;
+  const email = emailText.value;
+  const tel = telText.value;
+  const subject = subText.value;
+  let body = "" + bodyText.value + "%0A%0A%0A";
+
+  body += "Best Regards," + "%0A";
   body += "From：" + nameText.value + "%0A";
-  body += "Email：" + emailText.value + "%0A";
-  body += "Tel：" + telText.value;
-  //傳送的主要程式碼
+  body += "Email address：" + emailText.value + "%0A";
+  body += "Phone number：" + telText.value;
+
   mailTo.href = "mailto:" + to + "?subject=" + subject + "&body=" + body;
   mailTo.click();
 }
-//在body onload
+
 function init() {
   subText.value = initSubject;
   toText.value = initTo;
